ebdd7cf8034821db1dedd7dac93139f61403b86f
Michael VanderHelm <mikevh@gmail.com>
2016-06-13 17:10:24 -0700

    Adding identity ef

---
+++ Journey.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Journey.Web.Startup))]
+namespace Journey.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
