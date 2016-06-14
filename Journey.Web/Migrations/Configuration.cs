09bf7031752e1206c0828c0d19c108602bf8ad4e
Michael VanderHelm <mikevh@gmail.com>
2016-06-13 17:13:22 -0700

    Seed identity user

--- Journey.Web/Migrations/Configuration.cs
+++ Journey.Web/Migrations/Configuration.cs
@@ -1,3 +1,8 @@
+using System.Configuration;
+using Journey.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace Journey.Web.Migrations
 {
     using System;
@@ -26,6 +31,36 @@ namespace Journey.Web.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+    }
+
+    internal sealed class UserConfiguration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public UserConfiguration() {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        private string adminUsername => ConfigurationManager.AppSettings["adminUsername"] ?? "admin@admin.com";
+        private string adminPassword => ConfigurationManager.AppSettings["adminPassword"] ?? "Pass@word1";
+
+        protected override void Seed(ApplicationDbContext context) {
+            if (context.Users.Any(u => u.UserName == adminUsername)) {
+                return;
+            }
+            var adminuser = new ApplicationUser {
+                UserName = adminUsername,
+                PasswordHash = new PasswordHasher().HashPassword(adminPassword)
+            };
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var adminRole = new IdentityRole("Administrator");
+
+            roleManager.Create(adminRole);
+            userManager.Create(adminuser);
+            userManager.AddToRole(adminuser.Id, "Administrator");
         }
     }
 }
