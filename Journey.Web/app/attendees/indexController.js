42848c0eec5f19a07d350fd40842a8ccbee87366
mikevh <mikevh@gmail.com>
2016-09-10 20:18:18 -0700

    Adding filter to attendees list

--- Journey.Web/app/attendees/indexController.js
+++ Journey.Web/app/attendees/indexController.js
@@ -5,17 +5,45 @@
         .module('app.attendees')
         .controller('attendeesIndexController', controller);
 
-    controller.$inject = ['$route', 'attendeesService'];
+    controller.$inject = ['$route', 'attendeesService', 'communityGroupService'];
 
-    function controller($route, attendeesService) {
+    function controller($route, attendeesService, communityGroupService) {
         var vm = this;
         vm.remove = remove;
+
+        vm.clearFilter = function() {
+            vm.filterCommunityGroupId = -1;
+            vm.filterCommunityGroup();
+        };
+
+        vm.filterCommunityGroupId = -1;
+
+        vm.filterCommunityGroup = function () {
+            vm.attendees = _.filter(vm.all_attendees,
+                function (x) {
+                    if (vm.filterCommunityGroupId === -1) {
+                        return true;
+                    }
+                    if (vm.filterCommunityGroupId === 0 && x.communityGroupId == null) {
+                        return true;
+                    }
+                    return x.communityGroupId === vm.filterCommunityGroupId;
+                });
+        };
 
         activate();
 
         function activate() {
+
             attendeesService.all().then(function (result) {
+                vm.all_attendees = result.data;
                 vm.attendees = result.data;
+            });
+
+            communityGroupService.all().then(function (result) {
+                result.data.unshift({ id: 0, name: 'No group' });
+                result.data.unshift({ id: -1, name: '' });
+                vm.groups = result.data;
             });
         }
 
